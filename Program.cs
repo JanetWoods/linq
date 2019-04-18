12696303f664c6cc9ffb9100cdc2be34ad3b05f7
Janet Woods <janetnwoods@gmail.com>
2019-04-18 13:09:05 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace linqEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
